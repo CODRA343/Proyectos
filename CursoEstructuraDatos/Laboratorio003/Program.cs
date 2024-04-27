651ee7b5abfd9bf24bbdd449716cc1aeb89c2cd5
MIGUEL <migutimen_28@outlook.com>
2024-04-27 10:36:15 -0500

    a

---
+++ CursoEstructuraDatos/Laboratorio003/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var i = 10;
+
+var indice = 1;
+
+do
+{
+    Console.WriteLine(indice);
+    indice++;
+} while (indice <= i);
+
+//while (indice <= i)
+//{
+//    Console.WriteLine(indice);
+//    indice++;
+//}
+
+//for (var j = 1; j <=i; j++)
+//{
+//  Console.WriteLine(j);
+//}
+
+//var mensaje = i >= 4 ? "El número es mayor a 4!" : "El número es menor a 4!";
+
+//if (i >= 4)
+//{
+//    //positiva
+//    Console.WriteLine("El número es mayor a 4!");
+//}
+//else
+//{
+//    //Negativo
+//    Console.WriteLine("El número es menor a 4!");
+//}
